3a47439bae45aca47db43884acc6621953b0ad63
adrianshih <adrian20070729shih@gmail.com>
2024-02-19 18:59:08 +0800

    skills

--- public/src/pages/fight.js
+++ public/src/pages/fight.js
@@ -21,13 +21,26 @@ function handleWeaponChange() {
         document.getElementById("skill1Button").onclick = startpSkill1Animation;
         document.getElementById("skill2Button").addEventListener("click", function() {
             // 自動筆 is selected, so start skill 2 animation
-            startSkill2Animation();
+            startpSkill2Animation();
         });
-    } else {
+    } else if (selectedWeapon === "美工刀") {
+        // For 美工刀, set up skill 1 animation
+        document.getElementById("skill1Button").onclick = startkSkill1Animation;
+        document.getElementById("skill2Button").onclick = startkSkill2Animation;
+    } else if (selectedWeapon === "剪刀") {
+    // For 美工刀, set up skill 1 animation
+    document.getElementById("skill1Button").onclick = startsSkill1Animation;
+    document.getElementById("skill2Button").onclick = startsSkill2Animation;
+    } else if (selectedWeapon === "魔導書") {
+    // For 美工刀, set up skill 1 animation
+    document.getElementById("skill1Button").onclick = startbSkill1Animation;
+    document.getElementById("skill2Button").onclick = startbSkill2Animation;
+    }else {
         // For other weapons, remove the onclick handler for skill 1
         document.getElementById("skill1Button").onclick = null;
     }
 }
+
 
 
 // Function for 自動筆刀
@@ -55,7 +68,7 @@ function startpSkill1Animation() {
 }
 
 // Function for 自動筆 skill 2
-function startSkill2Animation() {
+function startpSkill2Animation() {
     var images = ['/images/bp21.png', '/images/bp22.png', '/images/bp23.png'];
     var student = document.querySelector('.student');
     var index = 0;
@@ -78,6 +91,144 @@ function startSkill2Animation() {
     }, 200 * (images.length + 1)); // Total duration for cycling through all images, plus a little extra
 }
 
+// Function for 美工刀 skill 1
+function startkSkill1Animation() {
+    var images = ['/images/bk21.png', '/images/bk22.png','/images/bk23.png','/images/bk24.png','/images/bk25.png'];
+    var student = document.querySelector('.student');
+    var index = 0;
+
+    var preloadedImages = [];
+    for (var i = 0; i < images.length; i++) {
+        preloadedImages[i] = new Image();
+        preloadedImages[i].src = images[i];
+    }
+
+    var intervalId = setInterval(function() {
+        student.style.backgroundImage = "url('" + preloadedImages[index].src + "')";
+        index = (index + 1) % images.length; // Cycle through the images
+    }, 200); // Switch character image every 0.2 seconds
+
+    // After cycling through the images, revert to the original picture
+    setTimeout(function() {
+        clearInterval(intervalId);
+        student.style.backgroundImage = "url('/images/walk 1.png')"; // Revert to original picture
+    }, 200 * (images.length + 1)); // Total duration for cycling through all images, plus a little extra
+}
+// Function for 美工刀 skill 2
+function startkSkill2Animation() {
+    var images = ['/images/bk11.png', '/images/bk12.png'];
+    var student = document.querySelector('.student');
+    var index = 0;
+
+    var preloadedImages = [];
+    for (var i = 0; i < images.length; i++) {
+        preloadedImages[i] = new Image();
+        preloadedImages[i].src = images[i];
+    }
+
+    var intervalId = setInterval(function() {
+        student.style.backgroundImage = "url('" + preloadedImages[index].src + "')";
+        index = (index + 1) % images.length; // Cycle through the images
+    }, 200); // Switch character image every 0.2 seconds
+
+    // After cycling through the images, revert to the original picture
+    setTimeout(function() {
+        clearInterval(intervalId);
+        student.style.backgroundImage = "url('/images/walk 1.png')"; // Revert to original picture
+    }, 200 * (images.length + 1)); // Total duration for cycling through all images, plus a little extra
+}
+// Function for 剪刀 skill 1
+function startsSkill1Animation() {
+    var images = ['/images/bs11.png', '/images/bs12.png','/images/bs13.png','/images/bs14.png'];
+    var student = document.querySelector('.student');
+    var index = 0;
+
+    var preloadedImages = [];
+    for (var i = 0; i < images.length; i++) {
+        preloadedImages[i] = new Image();
+        preloadedImages[i].src = images[i];
+    }
+
+    var intervalId = setInterval(function() {
+        student.style.backgroundImage = "url('" + preloadedImages[index].src + "')";
+        index = (index + 1) % images.length; // Cycle through the images
+    }, 200); // Switch character image every 0.2 seconds
+
+    // After cycling through the images, revert to the original picture
+    setTimeout(function() {
+        clearInterval(intervalId);
+        student.style.backgroundImage = "url('/images/walk 1.png')"; // Revert to original picture
+    }, 200 * (images.length + 1)); // Total duration for cycling through all images, plus a little extra
+}
+// Function for 剪刀 skill 2
+function startsSkill2Animation() {
+    var images = ['/images/bs11.png', '/images/bs12.png'];
+    var student = document.querySelector('.student');
+    var index = 0;
+
+    var preloadedImages = [];
+    for (var i = 0; i < images.length; i++) {
+        preloadedImages[i] = new Image();
+        preloadedImages[i].src = images[i];
+    }
+
+    var intervalId = setInterval(function() {
+        student.style.backgroundImage = "url('" + preloadedImages[index].src + "')";
+        index = (index + 1) % images.length; // Cycle through the images
+    }, 200); // Switch character image every 0.2 seconds
+
+    // After cycling through the images, revert to the original picture
+    setTimeout(function() {
+        clearInterval(intervalId);
+        student.style.backgroundImage = "url('/images/walk 1.png')"; // Revert to original picture
+    }, 200 * (images.length + 1)); // Total duration for cycling through all images, plus a little extra
+}
+// Function for 魔導書 skill 1
+function startbSkill1Animation() {
+    var images = ['/images/bb11.png', '/images/bb12.png', '/images/bb13.png', '/images/bb14.png', '/images/bb15.png', '/images/bb16.png', '/images/bb17.png', '/images/bb18.png', '/images/bb19.png', '/images/bb110.png', '/images/bb111.png', '/images/bb112.png', '/images/bb113.png', '/images/bb114.png', '/images/bb115.png', '/images/bb116.png'];
+    var student = document.querySelector('.student');
+    var index = 0;
+
+    var preloadedImages = [];
+    for (var i = 0; i < images.length; i++) {
+        preloadedImages[i] = new Image();
+        preloadedImages[i].src = images[i];
+    }
+
+    var intervalId = setInterval(function() {
+        student.style.backgroundImage = "url('" + preloadedImages[index].src + "')";
+        index = (index + 1) % images.length; // Cycle through the images
+    }, 200); // Switch character image every 0.2 seconds
+
+    // After cycling through the images, revert to the original picture
+    setTimeout(function() {
+        clearInterval(intervalId);
+        student.style.backgroundImage = "url('/images/walk 1.png')"; // Revert to original picture
+    }, 200 * (images.length + 1)); // Total duration for cycling through all images, plus a little extra
+}
+// Function for 魔導書 skill 2
+function startbSkill2Animation() {
+    var images = ['/images/bb21.png', '/images/bb22.png'];
+    var student = document.querySelector('.student');
+    var index = 0;
+
+    var preloadedImages = [];
+    for (var i = 0; i < images.length; i++) {
+        preloadedImages[i] = new Image();
+        preloadedImages[i].src = images[i];
+    }
+
+    var intervalId = setInterval(function() {
+        student.style.backgroundImage = "url('" + preloadedImages[index].src + "')";
+        index = (index + 1) % images.length; // Cycle through the images
+    }, 200); // Switch character image every 0.2 seconds
+
+    // After cycling through the images, revert to the original picture
+    setTimeout(function() {
+        clearInterval(intervalId);
+        student.style.backgroundImage = "url('/images/walk 1.png')"; // Revert to original picture
+    }, 200 * (images.length + 1)); // Total duration for cycling through all images, plus a little extra
+}
 // Function to handle the click event of the shield button
 document.getElementById("shieldButton").addEventListener("click", function() {
     // Call the function to toggle shield
